050c2dd924d4ce1b24d22b48c6ed63f7daf6f5ac
JoseL <jldominguez@proximity.es>
2018-02-28 12:26:42 +0100

    Añadidos carteles EXCLUSIVO HOTELES.COM

--- webspace/js/scripts.js
+++ webspace/js/scripts.js
@@ -1,3 +1,32 @@
+// Se lanza cuando se ha cargado la página
+window.onload = function(){
+	// console.log('Javascript cargado');
+
+	// Configuramos los elementos del DOM
+	setup_DOM();
+
+	// Configuramos JS como cargado
+	jscheck();
+
+	// Detectamos si el acceso se produce desde desde dispositivo móvil
+	mobilecheck();
+
+	// Activa funcionamiento de menú burguer
+	burguer();
+
+	// Elimina la clase de carga
+	removeClass(page.html, 'loading');
+
+	// Iguala hijos directos de elementos con clas .equalheight
+	equalheight(page.equal);
+};
+
+
+// Se lanza al redimensionar la ventana
+$(window).resize(function(){
+	equalheight(page.equal);
+	mobilecheck();
+});
 // Funciones para añadir y eliminar clases
 function addClass(elements, className) {
 	for (var i = 0; i < elements.length; i++) {
@@ -202,32 +231,3 @@ function logicaMasInfo(elems) {
         });
     });
 }
-// Se lanza cuando se ha cargado la página
-window.onload = function(){
-	// console.log('Javascript cargado');
-
-	// Configuramos los elementos del DOM
-	setup_DOM();
-
-	// Configuramos JS como cargado
-	jscheck();
-
-	// Detectamos si el acceso se produce desde desde dispositivo móvil
-	mobilecheck();
-
-	// Activa funcionamiento de menú burguer
-	burguer();
-
-	// Elimina la clase de carga
-	removeClass(page.html, 'loading');
-
-	// Iguala hijos directos de elementos con clas .equalheight
-	equalheight(page.equal);
-};
-
-
-// Se lanza al redimensionar la ventana
-$(window).resize(function(){
-	equalheight(page.equal);
-	mobilecheck();
-});
